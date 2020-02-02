c5af612c6138131fca7369ac5cbbc8ce3cbb11e3
Jason Miller <contact@jasonmiller.nl>
2020-02-02 23:59:17 +0100

    Reorganize serial functions

--- client/src/serial.rs
+++ client/src/serial.rs
@@ -7,34 +7,19 @@ use std::io::ErrorKind;
 
 use std::prelude::*;
 
-fn find_port(vid: u16, pid: u16) -> Result<String, ErrorKind> {
+pub fn serial_port((vid, pid): (u16, u16)) -> Result<String, serialport::ErrorKind> {
     if let Ok(ports) = serialport::available_ports() {
-        let mut port_name: String;
-
         for p in ports {
             match p.port_type {
-                SerialPortType::UsbPort(info) => {
-                    let pair = (info.vid, info.pid);
-
-                    match pair {
-                        (2341, 0043) => {
-                            return Ok(p.port_name);
-                        }
-                        _ => return Err(ErrorKind::NotFound),
+                SerialPortType::UsbPort(device) => {
+                    if (device.vid, device.pid) == (vid, pid) {
+                        return Ok(p.port_name);
                     }
                 }
-                _ => return Err(ErrorKind::NotFound),
+                _ => continue,
             }
         }
-
-        Ok("test".to_string())
-    } else {
-        return Err(ErrorKind::NotFound);
     }
-}
 
-fn main() {
-    let port = find_port(2341, 0043).unwrap();
-
-    println!("port: {}", port);
+    Err(serialport::ErrorKind::NoDevice)
 }
