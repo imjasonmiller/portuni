8c006aa20cc3143bbee59eadea5d68881971f7d8
Jason Miller <contact@jasonmiller.nl>
2020-02-09 19:14:02 +0100

    Add tests for MovingAverage

--- client/src/utils/interp.rs
+++ client/src/utils/interp.rs
@@ -2,14 +2,18 @@ pub fn lerp() {
     unimplemented!();
 }
 
-// TODO: Turn this into a generic array and not a vec!
+// TODO: Use a generic array for user defined length
 pub struct MovingAverage {
     queue: Vec<f64>,
     index: usize,
 }
 
 impl MovingAverage {
-    pub fn new(size: usize, initial_value: Option<f64>) -> MovingAverage {
+    pub fn new(array_size: usize, initial_value: Option<f64>) -> MovingAverage {
+        let size = match array_size {
+            0 => 1,
+            n => n,
+        };
         let value = match initial_value {
             Some(v) => v,
             None => 0.0,
@@ -30,5 +34,37 @@ impl MovingAverage {
 
     pub fn get_avg(&self) -> f64 {
         self.queue.iter().fold(0., |sum, n| sum + n) / (self.queue.len() as f64)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::*;
+
+    #[test]
+    fn test_moving_average() {
+        // A vec! of len() 0 is invalid, so we return one with len() 1
+        let result = MovingAverage::new(0, None);
+        assert_eq!(result.queue.len(), 1);
+
+        let result = MovingAverage::new(5, Some(1.0));
+        // The vec! should have 5 elements
+        assert_eq!(result.queue.len(), 5);
+        // All values should be 1.0f64
+        for n in result.queue.iter() {
+            assert_abs_diff_eq!(*n, 1.0);
+        }
+
+        let mut result = MovingAverage::new(2, Some(0.0));
+        // The average of 5.0 and 10.0 should equal 7.5
+        result.add(5.0);
+        result.add(10.0);
+        assert_abs_diff_eq!(result.get_avg(), 7.5 as f64);
+
+        // The average of 5.0 should equal 2.5
+        result.add(2.5);
+        result.add(2.5);
+        assert_abs_diff_eq!(result.get_avg(), 2.5 as f64);
     }
 }
